eeebfacfc1c0cfed8726dc86cb1a0bf03835cd7f
Cobra <742118448@qq.com>
2020-04-22 18:59:00 +0800

    代码diff结果添加行号
    新增

---
+++ coverage/ut-coverage/git-diff.sh
@@ -0,0 +1,28 @@
+
+#通过git diff 得到变更、新增代码的行号
+diff-lines() {
+    local path=
+    local line=
+    while read; do
+        esc=$'\033'
+        if [[ $REPLY =~ ---\ (a/)?.* ]]; then
+            continue
+        elif [[ $REPLY =~ \+\+\+\ (b/)?([^[:blank:]$esc]+).* ]]; then
+            path=${BASH_REMATCH[2]}
+        elif [[ $REPLY =~ @@\ -[0-9]+(,[0-9]+)?\ \+([0-9]+)(,[0-9]+)?\ @@.* ]]; then
+            line=${BASH_REMATCH[2]}
+        elif [[ $REPLY =~ ^($esc\[[0-9;]+m)*([\ +-]) ]]; then
+            echo "$path:$line:$REPLY"
+            if [[ ${BASH_REMATCH[2]} != - ]]; then
+                ((line++))
+            fi
+        fi
+    done
+}
+
+###
+#执行
+#git diff ${ommit_Id1}...${Commit_Id2} |diff-lines
+#示例 git diff  02342340er...HEAD |diff-lines
+
+
